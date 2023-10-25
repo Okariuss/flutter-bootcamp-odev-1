37284b8bdc257743bd61ccc51f54a9b4e9b79c54
Okan <orkunokann@gmail.com>
2023-10-25 15:52:22 +0300

    Değişkenler ödevi tamamlandı

---
+++ lib/degisken_odev/degisken_tanimlama.dart
@@ -0,0 +1,142 @@
+void main() {
+  String ilce = "Çankaya";
+  print("İlçe : $ilce");
+
+  String kita = "Avrupa";
+  print("Kıta : $kita");
+
+  String faks = "+901234567890@fax.tc";
+  print("Faks : $faks");
+
+  String postaKodu = "06660";
+  print("Posta Kodu : $postaKodu");
+
+  String instaAdresi = "orkunokann";
+  print("İnstagram Adresi : $instaAdresi");
+
+  String calistiginBolum = "Bilgisayar Mühendisi";
+  print("Çalıştığı Bölüm : $calistiginBolum");
+
+  int urunMiktari = 1;
+  print("Ürün Miktarı : $urunMiktari");
+
+  String musteriSoyadi = "Orkun";
+  print("Müşteri Soyadı : $musteriSoyadi");
+
+  double odemeMiktari = 149.99;
+  print("Ödeme Miktarı : $odemeMiktari");
+
+  String dogumTarihi = "9 Ağustos 1999";
+  print("Doğum Tarihi : $dogumTarihi");
+
+  double borcMiktari = 45.5;
+  print("Borç Miktarı : $borcMiktari");
+
+  bool evliMi = false;
+  print("Medeni Hali : $evliMi");
+
+  String videoYorumu = "Mükemmel bir anlatım";
+  print("Video Yorumu : $videoYorumu");
+
+  String odemeSaati = "19:00";
+  print("Ödeme Saati : $odemeSaati");
+
+  double eftMiktari = 75.75;
+  print("Eft Miktarı : $eftMiktari");
+
+  int satilanMiktar = 10;
+  print("Satılan Miktar : $satilanMiktar");
+
+  String telefonModeli = "iPhone";
+  print("Telefon Modeli : $telefonModeli");
+
+  String dergiAdi = "Bilim ve Gelecek";
+  print("Dergi Adı : $dergiAdi");
+
+  String basimTarihi = "16 Nisan 2003";
+  print("Basım Tarihi : $basimTarihi");
+
+  double zamMiktari = 12.5;
+  print("Zam Miktarı : $zamMiktari");
+
+  int daireSayisi = 5;
+  print("Daire Sayısı : $daireSayisi");
+
+  double enlem = 19.4341;
+  print("Enlem : $enlem");
+
+  double boylam = 45.3462;
+  print("Boylam : $boylam");
+
+  String yemekAdi = "Beyti Kebap";
+  print("Yemek Adı : $yemekAdi");
+
+  double urunFiyati = 33.75;
+  print("Ürün Fiyatı : $urunFiyati");
+
+  String sirket = "Getir";
+  print("Şirket : $sirket");
+
+  String videoAdi = "Flutter Bootcamp - Değişkenler";
+  print("Video Adı : $videoAdi");
+
+  double muzikSuresi = 3.49;
+  print("Müzik SÜresi : $muzikSuresi");
+
+  double mekanPuani = 3.9;
+  print("Mekan Puanı : $mekanPuani");
+
+  String dosyaAdi = "Değişkenler";
+  print("Dosya Adı : $dosyaAdi");
+
+  String resimFormati = ".jpg";
+  print("Resim Formatı : $resimFormati");
+
+  String renk = "Turuncu";
+  print("Renk : $renk");
+
+  String renkKodu = "#FF7F00";
+  print("Renk Kodu : $renkKodu");
+
+  String bilgisayarModeli = "Macbook Air";
+  print("Bilgisayar Modeli : $bilgisayarModeli");
+
+  double ekranBoyutu = 13.6;
+  print("Ekran Boyutu : $ekranBoyutu inç");
+
+  String kullanimSuresi = "3 saat 12 dakika";
+  print("Kullanım Süresi : $kullanimSuresi");
+
+  double basinc = 1.2;
+  print("Basınç : $basinc atm");
+
+  String etkinlikGunu = "25 Ekim 2023 Çarşamba";
+  print("Etkinlik Günü : $etkinlikGunu");
+
+  String odemeGunu = "26 Ekim 2023 Perşembe";
+  print("Ödeme Günü : $odemeGunu");
+
+  String yolculukCikisTarihi = "27 Ekim 2023 Cuma";
+  print("Yolculuk Çıkış Tarihi : $yolculukCikisTarihi");
+
+  String mahalleAdi = "Küçükesat";
+  print("Mahalle Adı : $mahalleAdi Mahallesi");
+
+  int otobusHatti = 600;
+  print("Otobüs Hattı : $otobusHatti");
+
+  double kullanilanDakika = 45.2;
+  print("Kullanılan Dakika : $kullanilanDakika");
+
+  int kargoTakipNo = 3048448681234;
+  print("Kargo Takip Numaranız : $kargoTakipNo");
+
+  int kuponSuresi = 3;
+  print("Kupon Süreniz : $kuponSuresi gün");
+
+  String kuponKodu = "TOY50";
+  print("Kupon Kodunuz : $kuponKodu");
+
+  String faturaTarihi = "13/06/2023";
+  print("$faturaTarihi tarihinde faturanız kesilmiştir.");
+}
